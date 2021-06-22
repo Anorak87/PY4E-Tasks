9c010b4b7ac16c5811bbaf9323ea7e6d2d14eba5
Cauã Pascoal <cauaproducoes@gmai.com>
2021-06-21 22:41:35 -0300

    Second Activitie

---
+++ Know_Your_Payment.py
@@ -0,0 +1,5 @@
+# %%
+#If you work and receive your payment per hour, this can be useful, at least for me to train :)
+hour = int(input('How mutch hours did you worked? '))
+per_h = int(input('How mutch do you receive per hour? '))
+print('You shuld expect find $', round(100*(hour*per_h))/100,' in your bank (or maybe in you pocket [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅])')
