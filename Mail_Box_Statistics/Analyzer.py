e8437cc264da7de0eca2e9ebb11294446e161868
Cauã Pascoal <cauaproducoes@gmai.com>
2021-07-02 22:01:57 -0300

    Sixth Task

--- Mail_Box_Statistics/Analyzer.py
+++ Mail_Box_Statistics/Analyzer.py
@@ -2,7 +2,7 @@ print("This time I will be counting the quantity of e-mails you get daily based 
 
 
 try:
-    fhandle = open('mail-box_wshort.txt')
+    fhandle = open('mail-box_short.txt')
 except:
     print("Oh man, here are you blowing up my code again. This time you will fix (▀̿Ĺ̯▀̿ ̿)")
     print("\nIf you didn't changed anything check how you are runing the code, please run python3 in the same folder were is located de file. VS Code, for an example, runs python archieves from your top hierarchy level all the way down and this wasn't working here. I tryed Jupyter extension and it got the right path. Never forget the old and good terminal ┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴")
